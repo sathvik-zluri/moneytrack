243adbfb37b933bac2511524b9ea1066f1059e22
sathvik-zluri <sathvik.k@zluri.com>
2025-01-23 00:33:08 +0530

    feat: add Footer component with styles for consistent layout

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer bg-dark text-light p-2">
+      <h6 className="text-center text-white">
+        All rights reserved &copy; MoneyTrack
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
